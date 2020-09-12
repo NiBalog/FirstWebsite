f58081f5450ef7a831f89486571bdd8242cfb8fd
Nicholas Balog <epicbalog@nicholass-mbp.lan>
2020-09-11 19:14:20 -0700

    final

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+var colorChanges = 0
 
 function setup() {
   document.getElementById("home-page").style.display = 'none';
@@ -26,6 +26,7 @@ function specs() {
 }
 
 function changeColor() {
+  if (colorChanges == 0) {
   document.getElementById("color-button").style.background = "white";
   document.getElementById("page-selection").style.background = "white";
   document.getElementById("page-button1").style.background = "#D3D3D3";
@@ -34,6 +35,7 @@ function changeColor() {
   document.getElementById("projectorList1").style.background = "white";
   document.getElementById("projectorList2").style.background = "white";
   document.getElementById("projectorList3").style.background = "white";
+  document.getElementById("table").style.background = "white";
   document.body.style.background = "#D3D3D3";
   var all = document.getElementsByTagName("*");
 
@@ -41,7 +43,27 @@ function changeColor() {
  all[i].style.color = "black";
 
 }
+colorChanges += 1
+}
+  else {
+    if (colorChanges == 1) {
+    document.getElementById("color-button").style.background = "black";
+    document.getElementById("page-selection").style.background = "#141414";
+    document.getElementById("page-button1").style.background = "#383838";
+    document.getElementById("page-button2").style.background = "#383838";
+    document.getElementById("page-button3").style.background = "#383838";
+    document.getElementById("projectorList1").style.background = "#141414";
+    document.getElementById("projectorList2").style.background = "#141414";
+    document.getElementById("projectorList3").style.background = "#141414";
+    document.getElementById("table").style.background = "#141414";
+    document.body.style.background = "#383838";
+    var all = document.getElementsByTagName("*");
 
+    for (var i=0, max=all.length; i < max; i++) {
+   all[i].style.color = "white";
 
-
+  }
+  colorChanges = 0
+}
+}
 }
